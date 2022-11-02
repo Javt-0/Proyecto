5d2bc5a87bf3d1b78909d5685109c3f789ccbf70
Javt-0 <jonathan96aguilar@gmail.com>
2022-11-02 21:58:06 +0100

    Creacion de clases

---
+++ Proyecto/src/main/java/datos/IAccesoDatos.java
@@ -0,0 +1,16 @@
+
+package datos;
+
+/**
+ *
+ * @author jonat
+ */
+public interface IAccesoDatos {
+    
+    public void crear();
+    
+    public void actualizar();
+    
+    public void buscar();
+    
+}
